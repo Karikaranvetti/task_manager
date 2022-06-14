99f590cfdf80aae16f22921b4ce59c274039b3dc
Karikaran vettivel <karikaran@paraqum.com>
2022-06-15 02:52:56 +0530

    api server fully completed and not wired with front end side

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import Login from './components/Login'
+
+ReactDOM.render(
+  <React.StrictMode>
+     <App /> 
+    {/* <Login/> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+ 
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+ 
